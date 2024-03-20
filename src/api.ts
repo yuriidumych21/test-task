a62fd72f18280fbfa66ed57f42e6a2f89baa69ff
ydumych <ydumyc@softserveinc.com>
2024-03-20 13:29:54 +0200

    added env file

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import { PexelsApiResponse } from './interfaces';
-const pexelsApiKey = 'hZokcFuLdbX3U8XmQCGxTlzkgQ94Q00zBhcA8r3Ugn21ZjNIoGcjWZB0';
 
 export const getImages = async (topic: string): Promise<PexelsApiResponse> => {
   const response = await axios.get<PexelsApiResponse>(`https://api.pexels.com/v1/search`, {
     headers: {
-      Authorization: pexelsApiKey
+      Authorization: process.env.REACT_APP_PEXELS_API_KEY
     },
     params: { query: topic, per_page: 5 }
   });
